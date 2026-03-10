7492d00d19692b1c1d0190e60559cc83bc81164f
rabit2022 <3101829204@qq.com>
2026-03-10 22:24:13 +0800

    0310    url调整到third

--- packages/nodejs/url/test/jsfl/test2.jsfl
+++
@@ -1,9 +0,0 @@
-// requirejs modules file test1.js
-require(['url'], function (_) {
-    const {fileURLToPath } =_;
-    // import {fileURLToPath} from "url";
-    const tempDir = AnJsflScript.folders.Log;
-
-    console.log(fileURLToPath(tempDir));
-
-});
---
+++ third/nodejs/url/index.ts
@@ -0,0 +1,53 @@
+/**
+ * @file: index.ts
+ * @author: 穹的兔兔
+ * @email: 3101829204@qq.com
+ * @date: 2026/1/25 21:10
+ * @project: AnJsflScript-ts
+ * @description:
+ * npx tsc --project tsconfig.json
+ * npx browserify lib/index.js -o out/url.jsfl --standalone url
+ */
+import * as url from "url";
+// import { parse } from "url";
+
+// fileURLToPath, pathToFileURL
+function fileURLToPath(url: string | URL): string {
+  // 在 .jsfl 中，url 一定是 string
+  // 如果未来在 Node.js 中调用，才可能是 URL
+  const urlString =
+    typeof url === "object" && url?.href ? (url as URL).href : (url as string);
+
+  return FLfile.uriToPlatformPath(urlString);
+}
+
+function pathToFileURL(path: string): URL {
+  let fileUri = FLfile.platformPathToURI(path);
+
+  // file:///C|/docs/file.txt
+  fileUri = fileUri.replace("|", ":");
+
+  // return new URL(fileUri);
+  // 源码得出parse返回的Uri就是URI对象
+  // @ts-ignore
+  return url.parse(fileUri);
+}
+
+// // 以下两个函数打包后没有
+// // @ts-ignore
+// url.fileURLToPath = fileURLToPath;
+// // @ts-ignore
+// url.pathToFileURL = pathToFileURL;
+
+// URL
+class aURL {
+  constructor(public urlString: string) {
+    // @ts-ignore
+    return url.parse(urlString);
+  }
+}
+
+export const URL = aURL;
+
+// module.exports = url;
+export { fileURLToPath, pathToFileURL };
